1981980f5a03a17767f8c3ce3222ef4998d78a87
Abigail Adegbiji <adegbijiabigail@gmail.com>
2024-11-09 16:17:47 -0600

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,5 @@
+-- TODO: add breadcrumbs to the statusbar https://github.com/SmiteshP/nvim-navic
+
 vim.opt.number = false
 vim.opt.mouse = "a"
 vim.opt.showmode = false
@@ -14,7 +16,8 @@ vim.opt.list = false
 vim.opt.cmdheight = 0
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
-vim.opt.fillchars = {eob = " "}
+vim.opt.fillchars = { eob = " " }
+vim.opt.tabstop = 4
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<M-m>", ":vsplit<CR>")
@@ -26,103 +29,121 @@ vim.keymap.set("n", "<M-k>", "<C-w><C-k>")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    error("Error cloning lazy.nvim:\n" .. out)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        error("Error cloning lazy.nvim:\n" .. out)
+    end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  "tpope/vim-sleuth",
-  { "lewis6991/gitsigns.nvim", opts = {} },
-  {
-    "kaicataldo/material.vim",
-    config = function()
-      vim.cmd.colorscheme("material")
-      vim.cmd.hi("Comment gui=none")
-      vim.cmd [[
-        highlight Normal guibg=none
-        highlight NonText guibg=none
-        highlight Normal ctermbg=none
-        highlight NonText ctermbg=none
-        highlight WinSeparator guifg=#546e7a ctermfg=145
-        highlight SignColumn guibg=none
-      ]]
-    end,
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({})
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<Space>ff", builtin.find_files)
-      vim.keymap.set("n", "<Space>fg", builtin.live_grep)
-      vim.keymap.set("n", "<Space>fb", builtin.buffers)
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    main = "nvim-treesitter.configs",
-    opts = {
-      ensure_installed = { "comment" },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = {},
-      },
-      indent = { enable = true, disable = {} },
-    }
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-      require('lualine').setup({
-        options = {
-          icons_enabled = false,
-          component_separators = { left = ' ', right = ' '},
-          section_separators = { left = ' ', right = ' '},
-          globalstatus = true,
-        },
-        sections = {
-          lualine_a = {'mode'},
-          lualine_b = {'branch', 'diff', 'diagnostics'},
-          lualine_c = {'filename'},
-          lualine_x = {}, lualine_y = {},
-          lualine_z = {'location'}
-        },
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    dependencies = {
-      "ms-jpq/coq_nvim", "ms-jpq/coq.artifacts",
-      "williamboman/mason-lspconfig.nvim", "williamboman/mason.nvim"
+    "tpope/vim-sleuth",
+    { "lewis6991/gitsigns.nvim", opts = {} },
+    {
+        --"kaicataldo/material.vim",
+        --"morhetz/gruvbox",
+        --"AlexvZyl/nordic.nvim",
+        "oxfist/night-owl.nvim",
+        config = function()
+            require("night-owl").setup({ bold = false, italics = false, transparent_background = true })
+            vim.cmd.colorscheme("night-owl")
+            vim.cmd.hi("Comment gui=none")
+            -- Transparency
+            vim.cmd [[
+              highlight Normal guibg=none
+              highlight NonText guibg=none
+              highlight Normal ctermbg=none
+              highlight NonText ctermbg=none
+              highlight SignColumn guibg=none
+            ]]
+        end,
     },
-    init = function()
-      vim.g.coq_settings = { auto_start = "shut-up" }
-    end,
-    config = function()
-      require("mason").setup({})
-      require("mason-lspconfig").setup({
-        ensure_installed = {"clangd", "rust_analyzer", "pyright", "gopls", "ts_ls"},
-      })
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("telescope").setup({})
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<Space>ff", builtin.find_files)
+            vim.keymap.set("n", "<Space>fg", builtin.live_grep)
+            vim.keymap.set("n", "<Space>fb", builtin.buffers)
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        main = "nvim-treesitter.configs",
+        opts = {
+            ensure_installed = { "comment" },
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = {},
+            },
+            indent = { enable = true, disable = {} },
+        }
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        config = function()
+            require('lualine').setup({
+                options = {
+                    icons_enabled = false,
+                    component_separators = { left = ' ', right = ' ' },
+                    section_separators = { left = ' ', right = ' ' },
+                    globalstatus = true,
+                },
+                sections = {
+                    lualine_a = { 'mode' },
+                    lualine_b = { 'branch', 'diff', 'diagnostics' },
+                    lualine_c = { 'filename' },
+                    lualine_x = {},
+                    lualine_y = {},
+                    lualine_z = { 'location' }
+                },
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        dependencies = {
+            "ms-jpq/coq_nvim", "ms-jpq/coq.artifacts",
+            "williamboman/mason-lspconfig.nvim", "williamboman/mason.nvim",
+            "lukas-reineke/lsp-format.nvim"
+        },
+        init = function()
+            -- No message from our auto comple plugin!
+            vim.g.coq_settings = { auto_start = "shut-up" }
+        end,
+        config = function()
+            local on_attach = function(client, buffer)
+                -- Autoformat
+                require("lsp-format").on_attach(client, buffer)
 
-      local lsp = require("lspconfig")
-      local coq = require("coq")
-      lsp.clangd.setup(coq.lsp_ensure_capabilities())
-      lsp.pyright.setup(coq.lsp_ensure_capabilities())
-      lsp.rust_analyzer.setup(coq.lsp_ensure_capabilities())
-      lsp.gopls.setup(coq.lsp_ensure_capabilities())
-      lsp.ts_ls.setup(coq.lsp_ensure_capabilities())
+                -- Disable semantic highlighting -- treesitter's enough
+                if client.server_capabilities.semanticTokensProvider then
+                    client.server_capabilities.semanticTokensProvider = nil
+                end
+            end
 
-      vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions)
-      vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references)
-      vim.keymap.set('n', 'gI', require('telescope.builtin').lsp_implementations)
-    end
-  }
+            local handlers = {
+                function(server_name) -- Handler for all lsp servers
+                    local coq = require("coq")
+                    local server = require("lspconfig")[server_name]
+                    server.setup(coq.lsp_ensure_capabilities({ on_attach = on_attach }))
+                end
+            }
+
+            require("mason").setup({})
+            require("mason-lspconfig").setup({
+                handlers = handlers,
+                ensure_installed = { "clangd", "rust_analyzer", "pyright", "gopls", "ts_ls" }
+            })
+
+            vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions)
+            vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references)
+            vim.keymap.set('n', 'gI', require('telescope.builtin').lsp_implementations)
+        end
+    }
 })
