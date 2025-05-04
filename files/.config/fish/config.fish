4dd286b9a4f60c8d094475a97d2e4f0a311339ec
Abigail Adegbiji <adegbijiabigail@gmail.com>
2025-05-04 13:53:58 -0600

    update

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -1,8 +1,16 @@
 set fish_greeting
-alias gdb="gdb -q"
+
 export EDITOR="nvim"
+
+alias vim="nvim"
+alias gdb="gdb -q"
 alias rm "rm -rf"
 alias cp "cp -r"
 alias ls "ls -a --color"
+
 alias setup "~/journal/open.sh && sudo pacman -Syu"
 alias journal "~/journal/journal.sh"
+
+export HACKATIME_API_KEY="ed21f62a-839f-407b-a73f-65541b5f48ae"
+export HACKATIME_API_URL="https://hackatime.hackclub.com/api/hackatime/v1"
+export SUCCESS_URL="https://hackatime.hackclub.com//success.txt"
