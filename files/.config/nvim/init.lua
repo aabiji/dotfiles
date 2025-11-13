d53803badb6597b4f770615b50cccab2e5f0d662
Abigail Adegbiji <adegbijiabigail@gmail.com>
2025-11-12 22:26:34 -0600

    update

--- files/.config/nvim/init.lua
+++ files/.config/nvim/init.lua
@@ -21,4 +21,206 @@ vim.keymap.set('n', '<C-k>', '<C-w>k')
 vim.keymap.set('n', '<C-l>', '<C-w>l')
 vim.keymap.set('n', '<C-m>', ':vsplit<CR>')
 vim.keymap.set('n', '<C-n>', ':split<CR>')
-vim.keymap.set('i', '<C-Backspace>', '<C-w>')
+vim.keymap.set('i', '<S-Backspace>', '<C-w>')
+vim.keymap.set('n', '<PageUp>', ':tabprev<CR>')
+vim.keymap.set('n', '<PageDown>', ':tabnext<CR>')
+
+-- remove trailing space on exit
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("RemoveTrailingWhitespace", { clear = true }),
+  callback = function()
+    vim.cmd([[keeppatterns %s/\s\+$//e]])
+  end,
+})
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local telescope = require('telescope')
+      local builtin = require('telescope.builtin')
+      local actions = require('telescope.actions')
+
+      telescope.setup({
+        defaults = {
+          border = false,
+          layout_strategy = 'bottom_pane',
+          layout_config = { height = 25 },
+        },
+        pickers = {
+          find_files = {
+            mappings = {
+              n = { ["<CR>"] = actions.select_tab },
+              i = { ["<CR>"] = actions.select_tab },
+            },
+          },
+        }
+      })
+
+      vim.keymap.set('n', '<leader>ff', builtin.find_files)
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+      vim.keymap.set('n', '<leader>fb', builtin.buffers)
+    end,
+  },
+
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+    },
+    config = function()
+      require('mason').setup()
+      require('mason-lspconfig').setup({ automatic_installation = true })
+
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local servers = { 'clangd', 'pyright', 'ts_ls', 'rust_analyzer', 'gopls', 'zls' }
+      for _, lsp in ipairs(servers) do
+        vim.lsp.config[lsp] = { capabilities = capabilities }
+      end
+
+      for _, lsp in ipairs(servers) do
+        vim.lsp.enable(lsp)
+      end
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local opts = { buffer = args.buf }
+          vim.keymap.set("n", "gd", "<cmd>tab split | lua vim.lsp.buf.definition()<CR>", {})
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'ge', vim.diagnostic.goto_next, opts)
+          vim.keymap.set('n', 'ga', vim.lsp.buf.rename, opts)
+
+        end,
+      })
+    end,
+  },
+
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+    },
+    config = function()
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<PageUp>'] = cmp.mapping.scroll_docs(-4),
+          ['<PageDown>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        }),
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'buffer' },
+          { name = 'path' },
+        },
+      })
+    end,
+  },
+
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        auth_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup()
+    end
+  },
+
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup({})
+    end,
+  },
+
+  {
+    'nmac427/guess-indent.nvim',
+    config = function()
+      require('guess-indent').setup({})
+    end,
+  },
+
+  {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      local bufferline = require("bufferline")
+      bufferline.setup({
+        options = {
+          style_preset = bufferline.style_preset.no_italic,
+          mode = "tabs",
+          show_buffer_close_icons = false,
+          show_tab_indicators = false,
+        }
+      })
+    end,
+  },
+
+  {
+    'morhetz/gruvbox',
+    config = function()
+      vim.cmd[[colo gruvbox]]
+      vim.opt.cursorline = true
+      vim.api.nvim_set_hl(0, "Normal", { bg = "#10151a" })
+      vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+      vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE" })
+      vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "NONE" })
+    end,
+  },
+})
