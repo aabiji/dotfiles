913fd8c2b5e1ba809cb9040b8a020fc21722a03a
Abigail <115902132+aabiji@users.noreply.github.com>
2026-01-29 08:31:13 -0600

    update

--- setup.sh
+++ setup.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-sudo dnf install -y git curl wget p7zip fish neovim ninja-build cmake ripgrep nodejs wl-clipboard gh gcc-c++ make
+sudo dnf install -y git curl wget p7zip-full fish ninja-build cmake ripgrep wl-clipboard gh build-essential make flatpak
+sudo snap remove firefox
 
-sudo dnf install -y flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.spotify.Client
 flatpak install flathub md.obsidian.Obsidian
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/mkasberg/ghostty-ubuntu/HEAD/install.sh)"
 curl -fsS https://dl.brave.com/install.sh | sh
 
-sudo dnf copr enable scottames/ghostty
-sudo dnf install ghostty -y
-
-sudo dnf rmeove firefox
+sudo apt-get install wget gpg &&
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg &&
+sudo install -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg &&
+rm -f microsoft.gpg
+sudo apt install apt-transport-https &&
+sudo apt update && sudo apt install code # or code-insiders
 
 # Setup GitHub auth and clone repos
+sudo snap install gh
 mkdir -p dev/archive
 cd ~/dev/archive
 gh auth login
 gh repo list aabiji --limit 1000 | awk '{print $1; }' | xargs -L1 gh repo clone
+sudo snap remove gh
 
 # Move journal and dotfiles
 cd ~
